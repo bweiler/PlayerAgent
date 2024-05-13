64adeaf97d5f60af1bc9db4cc02676a94d22ad91
bweiler <bill.bsee@gmail.com>
2024-05-13 04:50:01 +0100

    Roster file decompress, plus starting xml parser

--- src/agent/main.py
+++ src/agent/main.py
@@ -1,3 +1,19 @@
+import zipfile
+import xml.etree.ElementTree as ET
+
+with zipfile.ZipFile("../../example.rosz","r") as zip_ref:
+    zip_ref.extractall(".")
+
+tree = ET.parse('New Roster.ros')
+
+root = tree.getroot()
+print("Root is",root)
+
+for chards in root.iter('profile'):
+    for charres in chards.iter('characteristic'):
+         idi = charres.find('name').text
+         name = charres.find('typeId').text
+         print('Team (%s): %s' % (idi, name))    
 #Battle Rounds
 
 #Command Phase
