b120087b81658ad603c5de8f4d530f931245afb4
bweiler <bill.bsee@gmail.com>
2024-05-17 02:11:53 +0100

    Added morre xml parsing, logging, log cycling, new roster

--- src/agent/main.py
+++ src/agent/main.py
@@ -1,19 +1,59 @@
 import zipfile
 import xml.etree.ElementTree as ET
+import logging
+from datetime import datetime
 
-with zipfile.ZipFile("../../example.rosz","r") as zip_ref:
+class Modelk:
+    def __init__(self,strname)
+        self.strname = strname
+    def set_M(self, value):
+        self.Move = value
+    def set_T(self, value):
+        self.Toughness = value
+    def set_Sv(self, value):
+        self.SaveRoll = value
+    def set_W(self, value):
+        self.Wounds = value
+    def set_LD(self, value):
+        self.Leadership = value
+    def set_OC(self, value):
+        self.OccupyControl = value
+        
+compressed_rosterfile = '../../Necron Combat Patrol.rosz'
+rosteerfile = 'Necron Combat Patrol.ros'
+
+logf = logging.getLogger(__name__)
+logfilename = datetime.now().strftime('playeragent_%H_%M_%d_%m_%Y.log')
+logging.basicConfig(filename=logfilename, encoding='utf-8', level=logging.INFO)
+logf.info("Log Started")
+
+with zipfile.ZipFile(compressed_rosterfile,"r") as zip_ref:
     zip_ref.extractall(".")
 
-tree = ET.parse('New Roster.ros')
+tree = ET.parse(rosteerfile)
 
 root = tree.getroot()
-print("Root is",root)
+selections = root[1][0][0]
 
-for chards in root.iter('profile'):
-    for charres in chards.iter('characteristic'):
-         idi = charres.find('name').text
-         name = charres.find('typeId').text
-         print('Team (%s): %s' % (idi, name))    
+#print(selections)
+ns = {'wh': 'http://www.battlescribe.net/schema/rosterSchema'}
+
+names = { 'Overlord','Necron Warriors','Skorpekh Destroyers','Canoptek Scarab Swarms' }
+indexer = 0
+for selection in selections:
+    for getname in names:
+        print(getname)
+        
+        if selection.get('name',default='blank') == getname:
+            for profiles in selection:
+                for profile in profiles:
+                    if profile.get('name',default='blank') == getname:
+                        for characteristics in profile:
+                            for characteristic in characteristics:
+                                switch(characteristic.get('name',default='blank')
+                                        'M':
+                                            str(characteristic.text))
+             
 #Battle Rounds
 
 #Command Phase
