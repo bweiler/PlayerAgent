6fc780478b135ff385d6c929da460024f8c5820c
bweiler <bill.bsee@gmail.com>
2024-05-17 05:53:27 +0100

    initial parsing of roster xml and model data in class

--- src/agent/main.py
+++ src/agent/main.py
@@ -2,22 +2,40 @@ import zipfile
 import xml.etree.ElementTree as ET
 import logging
 from datetime import datetime
+import re
 
 class Modelk:
-    def __init__(self,strname)
+    def __init__(self,strname):
         self.strname = strname
     def set_M(self, value):
-        self.Move = value
+        tmp = re.findall(r'[^0–9]',value)
+        self.Move = int(tmp[0]) 
     def set_T(self, value):
-        self.Toughness = value
-    def set_Sv(self, value):
-        self.SaveRoll = value
+        tmp = re.findall(r'[^0–9]',value)
+        self.Toughness = int(tmp[0])
+    def set_Sv(self,value):
+        tmp = re.findall(r'[^0–9]',value)
+        self.SaveRoll = int(tmp[0])
     def set_W(self, value):
-        self.Wounds = value
+        tmp = re.findall(r'[^0–9]',value)
+        self.Wounds = int(tmp[0])
     def set_LD(self, value):
-        self.Leadership = value
+        tmp = re.findall(r'[^0–9]',value)
+        self.Leadership = int(tmp[0])
     def set_OC(self, value):
-        self.OccupyControl = value
+        tmp = re.findall(r'[^0–9]',value)
+        if len(tmp) == 0:
+            self.OccupyControl = 0
+        else:
+            self.OccupyControl = int(tmp[0])
+    def print_contents(self):
+        print(self.strname)
+        print("   M : " + str(self.Move))
+        print("   T : " + str(self.Toughness))
+        print("   Sv: " + str(self.SaveRoll))
+        print("   W : " + str(self.Wounds))
+        print("   LD: " + str(self.Leadership))
+        print("   OC: " + str(self.OccupyControl))
         
 compressed_rosterfile = '../../Necron Combat Patrol.rosz'
 rosteerfile = 'Necron Combat Patrol.ros'
@@ -39,20 +57,35 @@ selections = root[1][0][0]
 ns = {'wh': 'http://www.battlescribe.net/schema/rosterSchema'}
 
 names = { 'Overlord','Necron Warriors','Skorpekh Destroyers','Canoptek Scarab Swarms' }
-indexer = 0
+
+models = []
 for selection in selections:
     for getname in names:
-        print(getname)
-        
+        tmp = Modelk(getname)
         if selection.get('name',default='blank') == getname:
             for profiles in selection:
                 for profile in profiles:
                     if profile.get('name',default='blank') == getname:
                         for characteristics in profile:
                             for characteristic in characteristics:
-                                switch(characteristic.get('name',default='blank')
-                                        'M':
-                                            str(characteristic.text))
+                                match characteristic.get('name',default='blank'):
+                                        case 'M':
+                                            tmp.set_M(characteristic.text)
+                                        case 'T':
+                                            tmp.set_T(characteristic.text)
+                                        case 'SV':
+                                            tmp.set_Sv(characteristic.text)
+                                        case 'W':
+                                            tmp.set_W(characteristic.text)
+                                        case 'LD':
+                                            tmp.set_LD(characteristic.text)
+                                        case 'OC':
+                                            tmp.set_OC(characteristic.text)
+                            models.append(tmp)
+                            
+for obj in models:
+    obj.print_contents()
+                                        
              
 #Battle Rounds
 
